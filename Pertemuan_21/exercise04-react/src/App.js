8391a9669430d275b006d8fd4021d591d236c786
andregunawanrumoroy <s11810023@student.unklab.ac.id>
2020-11-17 16:15:17 +0700

    commit

--- Pertemuan_21/exercise04-react/src/App.js
+++ Pertemuan_21/exercise04-react/src/App.js
@@ -1,51 +1,74 @@
-import React from 'react';
-import './App.css';
+//pertemuan_24
 
-const sortByPower = (a, b) => {
-  return b.power - a.power;
+import React, { Component, useEffect, useState } from "react";
+import Card from "./components/Card";
+import "./App.css";
+import axios from 'axios';
+
+/*
+class App extends component{
+    state = {
+        users : [],
+    };
+
+    componentDidmount(){
+        //fetch("https://jsonplaceholder.typicode.com/users")
+        //.then(Response => Response.json())
+        //.then(json => this.setState({users : json}));
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((Response) => this.setState ({ users : Response.data }));
+    }
+
+    render(){
+        //tampilkan data
+        //console.log(this.state.users);
+        return <p>Class App Component</p>;
+
+        return (
+            <>
+              {this.state.users.map((item) => {
+                  return (
+                     <Card 
+                        name={Item.name}
+                        username={item.username}
+                        email={item.email}
+                        phone={item.phone}
+                        />
+                    );
+                })}
+            </>
+        )
+    }
 }
-const List = props => {
-  return (<>
-    <h1>React People!</h1>
-    <ul className="list">
-      {props.data.sort(sortByPower).map((person) => (
-        <li key={person.name} className="list-item">
-          <div className="list-item-image-container">
-            <img src={person.thumbnail} />
-          </div>
-          <span className="list-item-name">
-            {person.name} ( Power:{' '}
-            <span className="list-item-power">{person.power}</span> )
-          </span>
-        </li>
-      ))}
-    </ul>
-  </>
-  );
-}
+*/
 
 const App = () => {
-  const human = [
-    {
-      name: 'Krillin',
-      thumbnail:'https://upload.wikimedia.org/wikipedia/pt/6/63/Kuririn_42311.png',
-      power: 10
-    },  
-    {
-      name: 'Bulma',
-      thumbnail: 'https://upload.wikimedia.org/wikipedia/hu/1/1c/Bulma.png',
-      power: 3
-    },
-    {
-      name: 'Chi-chi',
-      thumbnail:'https://upload.wikimedia.org/wikipedia/hu/8/84/Chi-chi_dragon_ball_anime.jpg',
-      power: 30
-    }
-  ];
-  return (
-    <div className="App">
-      <List data={human} />
-    </div>
-  );
-}
+
+    const[users, setUsers] = useState([]);
+
+    useEffect (() => {
+        axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((Response) => setUsers (Response.data));
+    }, []);
+
+    //tampilakan -- console.log(users);
+
+    return (
+        <>
+            {users.map((item) => {
+                return (
+                    <Card
+                        name={Item.name}
+                        username={item.username}
+                        email={item.email}
+                        phone={item.phone} 
+                    />
+                );
+            })}
+        </>
+    );
+};
+
 export default App;
